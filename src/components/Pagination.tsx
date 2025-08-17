34422f788754e46e7c2916afefc4d258bba79047
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2025-08-17 16:47:12 +0600

    teacher page dynamic

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,20 +1,57 @@
-const Pagination = () => {
+"use client";
+import { ITEM_PER_PAGE } from "@/lib/settings";
+import { useRouter, useSearchParams } from "next/navigation";
+
+interface PaginationProps {
+  count: number;
+  page: number;
+}
+
+const Pagination = ({ count, page }: PaginationProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const totalPages = Math.ceil(count / ITEM_PER_PAGE);
+
+  const handlePageChange = (newPage: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", newPage.toString());
+    router.push(`?${params.toString()}`);
+  };
+
   return (
     <div className="p-4 flex items-center justify-between text-gray-500">
       <button
-        disabled
+        disabled={page <= 1}
+        onClick={() => handlePageChange(page - 1)}
         className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
       >
         Prev
       </button>
+      
       <div className="flex items-center gap-2 text-sm">
-        <button className="px-2 rounded-sm bg-lamaSky">1</button>
-        <button className="px-2 rounded-sm ">2</button>
-        <button className="px-2 rounded-sm ">3</button>
-        ...
-        <button className="px-2 rounded-sm ">10</button>
+        {Array.from({ length: totalPages }).map((_, index) => {
+          const pageIndex = index + 1;
+          return (
+            <button
+              key={pageIndex}
+              onClick={() => handlePageChange(pageIndex)}
+              className={`px-2 rounded-sm ${
+                page === pageIndex
+                  ? "bg-lamaBlue text-white"
+                  : "bg-lamaSky hover:bg-lamaBlue/50"
+              }`}
+            >
+              {pageIndex}
+            </button>
+          );
+        })}
       </div>
-      <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+
+      <button
+        disabled={page >= totalPages}
+        onClick={() => handlePageChange(page + 1)}
+        className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+      >
         Next
       </button>
     </div>
